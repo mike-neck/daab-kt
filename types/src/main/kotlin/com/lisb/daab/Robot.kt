515f7428502568512219cee7e18164e47a20f78f
mike_neck <jkrt3333@gmail.com>
2017-06-28 23:45:33 +0900

    add hear : reaction to group talk.

--- types/src/main/kotlin/com/lisb/daab/Robot.kt
+++ types/src/main/kotlin/com/lisb/daab/Robot.kt
@@ -27,8 +27,24 @@ external interface Robot {
     fun listen(regex: Regex, callback: (Response) -> Unit): Unit
     fun listen(regex: Regex, options: ListenerOption, callback: (Response) -> Unit): Unit
 
+    // hear : reaction to group talk.
+
     fun hear(regex: Regex, callback: (Response) -> Unit): Unit
     fun hear(regex: Regex, options: ListenerOption, callback: (Response) -> Unit): Unit
+
+    fun hear(stamp: Receive.Stamp, action: (ActionResponse<Stamp>) -> Unit): Unit
+
+    fun hear(question: Receive.YesNo, action: (ActionResponse<ReceivedQuestion>) -> Unit): Unit
+
+    fun hear(select: Receive.Select, action: (ActionResponse<ReceivedSelect>) -> Unit)
+
+    fun hear(task: Receive.Task, action: (ActionResponse<ReceivedTask>) -> Unit): Unit
+
+    fun hear(file: Receive.File, action: (ActionResponse<ReceivedFile>) -> Unit): Unit
+    fun hear(files: Receive.FileWithMessage, action: (ActionResponse<ReceivedFilesWithMessage>) -> Unit): Unit
+    fun hear(files: Receive.Files, action: (ActionResponse<ReceivedFiles>) -> Unit): Unit
+
+    // respond : reaction to pair talk.
 
     fun respond(regex: Regex, callback: (Response) -> Unit): Unit
 
