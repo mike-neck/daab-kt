0b3e5b97d4cb395395d1a40fef6c680ce3eefdd2
mike_neck <jkrt3333@gmail.com>
2017-07-12 23:31:20 +0900

    add binding module.

---
+++ types/src/main/resources/daab-kt-js-binding.js
@@ -0,0 +1,10 @@
+// module daab-kt-js-binding
+module.exports = (() => {
+    "use strict";
+    let object = {};
+    object.receive |= {};
+    let receive = object.receive;
+    ["stamp", "yesno", "select", "task", "file", "files", "map"]
+        .forEach(item => receive[item] = item);
+    return object;
+})();
